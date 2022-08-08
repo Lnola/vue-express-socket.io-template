935a74e885a70707d74cd265574d690054afaed4
Lnola <nola.luka@gmail.com>
2022-08-08 10:25:49 +0200

    Add initial frontend setup

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,28 @@
-<template></template>
+<template>
+  <p v-for="{ content } in messages">{{ content }}</p>
+  <form @submit.prevent="handleSubmit">
+    <input v-model="input" type="text" />
+    <input type="submit" />
+  </form>
+</template>
 
-<script setup></script>
+<script setup>
+import { onMounted, ref } from "vue";
+import socket from "./socket/socket.service";
 
-<style scoped></style>
+const messages = ref([]);
+const input = ref("");
+
+const handleSubmit = () => {
+  input.value = "";
+};
+</script>
+
+<style>
+body {
+  height: 100vh;
+  width: 100vw;
+  background-color: black;
+  color: white;
+}
+</style>

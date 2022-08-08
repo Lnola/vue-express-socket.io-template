abf9316db687706b440c8e9da5be8350a781a065
Lnola <nola.luka@gmail.com>
2022-08-08 10:31:03 +0200

    Add documentation comments to the code addressing it

--- server.js
+++ server.js
@@ -4,12 +4,21 @@ import { Server } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
+
+// documentation for handling cookies -> https://socket.io/how-to/deal-with-cookies
 const io = new Server(server, {
   cors: {
     origins: ["http://localhost:4000"],
   },
 });
 
+// middleware documentation -> https://socket.io/docs/v4/middlewares/
+io.use((socket, next) => {
+  // middleware function
+  next();
+});
+
+// basic communication documentation -> https://socket.io/docs/v4/server-socket-instance/
 io.on("connection", (socket, next) => {
   console.log("A user connected!");
 
